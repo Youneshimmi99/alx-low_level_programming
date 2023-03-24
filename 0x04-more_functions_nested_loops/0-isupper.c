8dd2e66feabc1100d2792464be550a91ce45687f
younes himmi <himmiyounes99@gmail.com>
2023-03-24 15:08:21 -0700

    fix isupper

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,16 +1,21 @@
-#include "holberton.h"
-
-/**
- * _isupper - checks is a character is uppercase
- * @c: carrier integer variable
- * Return: 1 if true, 0 if false
+/*
+ * File: 0-isupper.c
+ * Auth: Ephrem Abebe
  */
 
+#include "main.h"
+
+/**
+ * _isupper - Checks for uppercase characters.
+ * @c: The character to be checked.
+ *
+ * Return: 1 if character is uppercase, 0 otherwise.
+ */
 int _isupper(int c)
 {
-	if (c >= 65 && c <= 90)
-	{
+	if (c >= 'A' && c <= 'Z')
 		return (1);
-	}
-	return (0);
+
+	else
+		return (0);
 }
