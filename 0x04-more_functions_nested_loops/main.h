b645e93bdf705f181b0f3c05e98b001cceae255c
younes himmi <himmiyounes99@gmail.com>
2023-03-24 15:16:05 -0700

    add main.h

---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,14 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/*
+ * File: main.h
+ * Auth: Younes himmi
+ * Desc: Header file containing prototypes for functions
+ *       used in the 0x03-debugging.
+ */
+
+int _putchar(char c);
+int _isupper(int c);
+
+#endif
