95e31c255fb3668812d5a6394569253661955024
younes himmi <himmiyounes99@gmail.com>
2023-03-27 16:20:38 -0700

    fix puts2

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -3,19 +3,17 @@
 /**
  * puts2 - print other char of a string
  * @str: string reference
- * Return: null
+ * Return: void
  */
 
 void puts2(char *str)
 {
 	int i = 0;
 
-	while (str[i] != '\0')
+	while (*(str + i) != '\0')
 	{
 		if (i % 2 == 0)
-		{
-			_putchar(str[i]);
-		}
+			_putchar(*(str + i));
 		i++;
 	}
 	_putchar('\n');
