957d06444f71d96f79f453e2c0436c0c0a427c51
younes himmi <himmiyounes99@gmail.com>
2023-03-27 16:17:22 -0700

    puts2

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+
+/**
+ * puts2 - print other char of a string
+ * @str: string reference
+ * Return: null
+ */
+
+void puts2(char *str)
+{
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+		if (i % 2 == 0)
+		{
+			_putchar(str[i]);
+		}
+		i++;
+	}
+	_putchar('\n');
+}
