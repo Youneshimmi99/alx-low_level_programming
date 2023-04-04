6e678029a4c3c2e3484edd46c6efc4cfea22f834
younes himmi <himmiyounes99@gmail.com>
2023-04-03 19:06:10 -0700

    fix memcpy

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -1,19 +1,17 @@
-/**
- * _memcpy - Copies 'n' bytes from memory area 'src' to memory area 'dest'.
- * @dest: Pointer to the destination array to copy to.
- * @src: Pointer to the source array to copy from.
- * @n: Number of bytes to copy.
- *
- * Return: Pointer to the destination array.
+/*
+ * This function copies n bytes from memory area src to memory area dest.
+ * The memory areas may overlap.
+ * Returns a pointer to dest.
  */
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
     unsigned int i;
 
-    for (i = 0; i < n; i++)
-    {
-        dest[i] = src[i];
+    /* Copy bytes from src to dest */
+    for (i = 0; i < n; i++) {
+        *(dest + i) = *(src + i);
     }
 
+    /* Return a pointer to the start of dest */
     return dest;
 }
