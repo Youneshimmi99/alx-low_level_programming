69a4288f1aaa5094e4b0cc4d7a7bc8c116b7a764
younes himmi <himmiyounes99@gmail.com>
2023-04-03 19:28:15 -0700

    fix strspn

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -1,38 +1,30 @@
 #include "main.h"
 
 /**
- * This function gets the length of the initial substring in s that consists
- * entirely of characters from accept.
+ * _strspn - get length of a prefix substring
  *
- * @param s The string to work on.
- * @param accept The pattern to match against.
- * @return An unsigned integer representing the number of initial bytes in s that match the pattern.
+ * @s: string to work on
+ * @accept: pattern it will match  against
+ * Return: how many initial bytes match the pattern
  */
 unsigned int _strspn(char *s, char *accept)
 {
-    unsigned int i, j, len = 0, match = 0;
+	unsigned int i, j, len = 0, match = 0;
 
-    // iterate through each character in s
-    for (i = 0; s[i] != '\0'; i++)
-    {
-        // iterate through each character in accept
-        for (j = 0; accept[j] != '\0'; j++)
-        {
-            // if the current character in s matches the current character in accept, increment len
-            if (s[i] == accept[j])
-            {
-                len++;
-                break;
-            }
-        }
-        // if len is not 0, increment match
-        if (len != 0)
-            match++;
-        // if match is not equal to len, return len
-        if (match != len)
-            return (len);
-    }
-    // if no matches were found, return 0
-    return (0);
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; accept[j] != '\0'; j++)
+		{
+			if (s[i] == accept[j])
+			{
+				len++;
+				break;
+			}
+		}
+		if (len != 0)
+			match++;
+		if (match != len)
+			return (len);
+	}
+	return (0);
 }
-
