f7cad75b5a71090efd306bee26e65e63849f7a80
younes himmi <himmiyounes99@gmail.com>
2023-04-03 19:37:51 -0700

    fix ststs

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -9,26 +9,26 @@
  */
 char *_strstr(char *haystack, char *needle)
 {
-    int i, j, match;
+	int i, j, match;
 
-    if (*needle == '\0')
-        return (haystack);
-    for (i = 0; haystack[i] != '\0'; i++)
-    {
-        if (haystack[i] == *needle)
-        {
-            for (j = 1; needle[j] != '\0'; j++)
-            {
-                if (needle[j] != haystack[i + j])
-                {
-                    match = 0;
-                    break;
-                }
-                match = 1;
-            }
-            if (match)
-                return (haystack + i);
-        }
-    }
-    return (NULL);
+	if (*needle == '\0')
+		return (haystack);
+	for (i = 0; haystack[i] != '\0'; i++)
+	{
+		if (haystack[i] == *needle)
+		{
+			for (j = 1; needle[j] != '\0'; j++)
+			{
+				if (needle[j] != haystack[i + j])
+				{
+					match = 0;
+					break;
+				}
+				match = 1;
+			}
+			if (match)
+				return (haystack + i);
+		}
+	}
+	return (NULL);
 }
