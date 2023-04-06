ea4f53ae27c6fdcd38b993a5df9e71a7174264ae
younes himmi <himmiyounes99@gmail.com>
2023-04-06 15:18:21 -0700

    fix betty style strlen recursion

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -11,5 +11,5 @@ int _strlen_recursion(char *s)
 	if (*s == '\0') /* base case */
 		return (0);
 	else
-		return (1 + _strlen_recursion(s + 1)); /* recursive call for next character */
+		return (1 + _strlen_recursion(s + 1));
 }
