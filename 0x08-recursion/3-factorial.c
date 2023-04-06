8a6a71bbb06df4b8befb569b2370e39157e4fa50
himmiyounes <youneshimmi@hotmail.fr>
2023-04-06 23:07:59 +0000

    refactor factorial

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -8,16 +8,9 @@
  */
 int factorial(int num)
 {
-    if (num < 0)
-    {
-        return -1;
-    }
-    else if (num == 0)
-    {
-        return 1;
-    }
-    else
-    {
-        return num * factorial(num - 1);
-    }
+	if (num < 0)
+		return (-1);
+	else if (num == 0)
+		return (1);
+	return (num * factorial(num - 1));
 }
