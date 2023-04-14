63fec47aa4f0041b9a341fb0efa72bbcd38eef65
himmiyounes <youneshimmi@hotmail.fr>
2023-04-14 02:34:46 +0000

    remove description array range

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -7,11 +7,6 @@
  * @max: The largest number to be stored in the array
  *
  * Return: A pointer to the newly created array, or NULL if the function fails
- *
- * This function creates an array of integers containing a range of values from
- * min to max, both inclusive. If min is greater than max, the function returns
- * NULL. If the function fails to allocate memory for the array, it also returns
- * NULL. Otherwise, the function returns a pointer to the newly created array.
  */
 int *array_range(int min, int max)
 {
