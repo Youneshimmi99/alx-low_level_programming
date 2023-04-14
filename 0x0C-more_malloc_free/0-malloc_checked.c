125aa3781833c4e4cd3d6b500ed6e5774ba7b1f8
himmiyounes <youneshimmi@hotmail.fr>
2023-04-14 01:41:27 +0000

    chnge b to i

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -4,16 +4,15 @@
 /**
  * malloc_checked - Allocates memory using malloc and
  * checks for errors
- * @b: The number of bytes to allocate
+ * @i: The number of bytes to allocate
  *
  * Return: A pointer to the allocated memory, or exit with 
  * status 98 on failure
  */
 void *malloc_checked(unsigned int i)
 {
-	void *k;
+	void *k = malloc(i);
 
-	k = malloc(i);
 	if (k == NULL)
 		exit(98);
 	return (k);
