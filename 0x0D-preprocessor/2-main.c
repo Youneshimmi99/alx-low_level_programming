e720ffdd9e8e99acd517a88cd9ae7b159f63b033
himmiyounes <youneshimmi@hotmail.fr>
2023-04-17 23:23:06 +0000

    remove description

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,10 +1,8 @@
 #include <stdio.h>
 /**
  * main - Entry point of the program
- * Description: This function prints the name of 
- * the file it was compiled from
  *
- * Return: Always 0 (Success).
+ * Return: Always 0.
  */
 int main(void)
 {
