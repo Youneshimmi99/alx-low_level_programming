4be3e153517a453b5d8e7c13bc1c7fa8862ca879
himmiyounes <youneshimmi@hotmail.fr>
2023-04-17 23:26:51 +0000

    function_like_macro

---
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -0,0 +1,10 @@
+#ifndef LIKEMACRO_H
+#define LIKEMACRO_H
+/**
+ * File: 3-function_like_macro.h
+ * @A: input parameter to the ABS macro
+ * Desc: header file
+ */
+#define ABS(A) ((A) < 0 ? -(1) * (A) : (A))
+
+#endif
