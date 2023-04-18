a32e16268ce31eb8626e2fc6c688887a32592628
himmiyounes <youneshimmi@hotmail.fr>
2023-04-18 00:23:04 +0000

    add strncpy and strlen to dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -17,5 +17,7 @@ void init_dog(struct dog *d, char *dog_name, float dog_age, char *dog_owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *dog_name, float dog_age, char *dog_owner);
 void free_dog(dog_t *d);
+char *_strncpy(char *dest, char *src, int n);
+int _strlen(char *s);
 
 #endif
