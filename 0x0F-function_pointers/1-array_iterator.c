a1f03442bc20f9f0fa8f70d267f38aec24138ba4
himmiyounes <youneshimmi@hotmail.fr>
2023-04-19 22:03:35 +0000

    fix indentation in array_iterator

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -6,18 +6,21 @@
  * @arr: pointer to the array
  * @arr_size: size of the array
  * @apply_func: pointer to the function to apply to each element
+ *
+ * This function takes a pointer to an array, its size, and a pointer to a
+ * function. It applies the function to each element of the array in turn.
  */
 void array_iterator(int *arr, size_t arr_size, void (*apply_func)(int))
 {
-    size_t n;
+size_t n;
 
-    if (arr == NULL || apply_func == NULL)
-    {
-        return;
-    }
+if (arr == NULL || apply_func == NULL)
+{
+    return;
+}
 
-    for (n = 0; n < arr_size; n++)
-    {
-        apply_func(arr[n]);
-    }
+for (n = 0; n < arr_size; n++)
+{
+    apply_func(arr[n]);
+}
 }
