be6cd5f39849b2a86a8cc88b7ac2d838f9cabe2f
himmiyounes <youneshimmi@hotmail.fr>
2023-04-19 22:02:33 +0000

    remove description from array_iterator

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -6,9 +6,6 @@
  * @arr: pointer to the array
  * @arr_size: size of the array
  * @apply_func: pointer to the function to apply to each element
- *
- * This function takes a pointer to an array, its size, and a pointer to a
- * function. It applies the function to each element of the array in turn.
  */
 void array_iterator(int *arr, size_t arr_size, void (*apply_func)(int))
 {
