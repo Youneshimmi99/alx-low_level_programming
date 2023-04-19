f06bae5abc6aaad44554f6cabf383fd704441312
himmiyounes <youneshimmi@hotmail.fr>
2023-04-19 22:11:59 +0000

    add int_index to function_pointers.h

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -3,5 +3,6 @@
 
 void print_name(char *name, void (*printer)(char *));
 void array_iterator(int *arr, size_t arr_size, void (*apply_func)(int));
+int int_index(int *arr, int arr_size, int (*compare)(int));
 
 #endif
