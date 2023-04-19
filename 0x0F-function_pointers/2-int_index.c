ecebd3c4ef5c9fb3d29b2de8c1189d9b0a196ab9
himmiyounes <youneshimmi@hotmail.fr>
2023-04-19 22:11:06 +0000

    add int_index

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include "function_pointers.h"
+/**
+ * int_index - searches for an integer in an array using a comparison function
+ * @arr: pointer to the array of integers
+ * @arr_size: number of elements in the array
+ * @compare: pointer to the comparison function
+ *
+ * This function searches for an integer in an array by applying a comparison
+ * function to each element of the array. The comparison function should return
+ * non-zero if the element matches the desired integer, and zero otherwise. If
+ * the integer is found, the function returns its index in the array. If not,
+ * the function returns -1.
+**/
+int int_index(int *arr, int arr_size, int (*compare)(int))
+{
+	int n;
+
+	if (arr == NULL || compare == NULL)
+	{
+		return (-1);
+	}
+
+	if (arr_size <= 0)
+	{
+		return (-1);
+	}
+
+	for (n = 0; n < arr_size; n++)
+	{
+		if (compare(arr[n]))
+		{
+			return (n);
+		}
+	}
+
+	return (-1);
+}
