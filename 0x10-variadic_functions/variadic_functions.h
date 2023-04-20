4f16dedb99d7d904b3ca9a84ee0b5757f1324be3
himmiyounes <youneshimmi@hotmail.fr>
2023-04-20 22:54:42 +0000

    edit variadic_functions.h

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -6,5 +6,6 @@
 int sum_them_all(const unsigned int num_args, ...);
 void print_numbers(const char *delimiter, const unsigned int num_args, ...);
 void print_strings(const char *delimiter, const unsigned int num_args, ...);
+void print_all(const char * const type_string, ...);
 
 #endif
