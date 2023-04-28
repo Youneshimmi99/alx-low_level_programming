edb3e79139a35c9132ab2c80a470ab99049c7493
Youneshimmi99 <himmiyounes99@gmail.com>
2023-04-28 01:54:01 +0100

    fix logic

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -16,9 +16,9 @@ size_t print_list(const list_t *head)
 	while (head != NULL)
 	{
 		if (head->str == NULL)
-			printf("[%d] %s\n", 0, "(nil)");
+			printf("[0] (nil)\n");
 		else
-			printf("[%d] %s\n", head->len, head->str);
+			printf("[%u] %s\n", head->len, head->str);
 		head = head->next;
 		count++;
 	}
