6ebc89853b38fd9511b849658e895f262d17cd9c
Youneshimmi99 <himmiyounes99@gmail.com>
2023-04-28 01:52:34 +0100

    fix comments styles

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -4,10 +4,10 @@
 #include "lists.h"
 
 /**
-* print_list - Prints elements of linked list
-* @head: Pointer to the head of the list
-* Return: Count number
-*/
+ * print_list - Prints elements of linked list
+ * @head: Pointer to the head of the list
+ * Return: Count number
+**/
 
 size_t print_list(const list_t *head)
 {
