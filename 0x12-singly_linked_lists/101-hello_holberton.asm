d508102ea7813d2a2634062226b43310a2ef02fd
Youneshimmi99 <himmiyounes99@gmail.com>
2023-04-29 02:20:15 +0100

    add comments

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,6 +1,9 @@
+; File: 101-hello_holberton.asm
+; Desc: Print Hello, Holberton followed by a new line.
+
 section .data
-    msg db 'Hello, Holberton', 0
-    fmt db '%s\n', 0
+msg:	db "Hello, Holberton", 0
+fmt:	db "%s", 10, 0
 
 section .text
     global main
