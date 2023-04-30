b881f9692d2287d53f3fa9e0c2cf526df4a03b49
Youneshimmi99 <himmiyounes99@gmail.com>
2023-04-30 23:47:18 +0100

    refactor 0-print_listint

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,5 +1,4 @@
 #include "lists.h"
-#include <stdio.h>
 
 /**
  * print_listint - Prints the elements of a linked list.
@@ -11,12 +10,11 @@ size_t print_listint(const listint_t *h)
 {
 	size_t nodes = 0;
 
-	while (h)
+	while (h != NULL)
 	{
-		nodes++;
 		printf("%d\n", h->n);
 		h = h->next;
+		nodes++;
 	}
-
 	return (nodes);
 }
