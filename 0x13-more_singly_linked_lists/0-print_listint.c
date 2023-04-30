0cca06e1cdb329525dc9e5cbb830285aa300a4fd
Youneshimmi99 <himmiyounes99@gmail.com>
2023-04-30 23:33:21 +0100

    change name of variable

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -8,13 +8,14 @@
  */
 size_t print_listint(const listint_t *h)
 {
-	size_t numNodes = 0;
+	size_t nodes = 0;
 
-	while (h != NULL)
+	while (h)
 	{
+		nodes++;
 		printf("%d\n", h->n);
 		h = h->next;
-		numNodes++;
 	}
-	return (numNodes);
+
+	return (nodes);
 }
