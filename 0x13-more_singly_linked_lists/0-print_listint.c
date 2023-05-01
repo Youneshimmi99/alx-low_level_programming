a9fd95261870a574bd4d0846386bbdf8adc4c4ee
Youneshimmi99 <himmiyounes99@gmail.com>
2023-05-01 23:39:17 +0100

    fix comment

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,10 +1,11 @@
 #include "lists.h"
 
 /**
- * print_listint - Print elements of linked list.
- * @h: Pointer to the head of the linked list.
- * Return: The number of nodes in the linked list.
- */
+  * print_listint - Print elements of linked list
+  * @h: Pointer to the head of the linked list
+  *
+  * Return: The number of nodes in the linked list
+  */
 size_t print_listint(const listint_t *h)
 {
 	int count;
