9aec00b9c55eef1201e1695ea70ced494169b9b8
Youneshimmi99 <himmiyounes99@gmail.com>
2023-05-01 23:56:46 +0100

    add main_opcode

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,48 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * print_opcodes - Prints the opcodes of a given function
+ * @a: The address of the function
+ * @n: The number of bytes to print
+ *
+ * Return: void
+ */
+void print_opcodes(char *a, int n)
+{
+    int i;
+
+    for (i = 0; i < n; i++)
+    {
+        printf("%.2hhx", a[i]);
+        if (i < n - 1)
+            printf(" ");
+    }
+    printf("\n");
+}
+
+/**
+ * main - Prints the opcodes of its own main function
+ * @argc: The number of arguments passed to the program
+ * @argv: An array of pointers to the arguments
+ *
+ * Return: Always 0
+ */
+int main(int argc, char **argv)
+{
+    int n;
+
+    if (argc != 2)
+    {
+        printf("Error\n");
+        exit(1);
+    }
+    n = atoi(argv[1]);
+    if (n < 0)
+    {
+        printf("Error\n");
+        exit(2);
+    }
+    print_opcodes((char *)&main, n);
+    return (0);
+}
