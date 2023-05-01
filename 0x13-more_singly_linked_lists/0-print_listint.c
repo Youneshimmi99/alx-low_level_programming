6d99fdedd85ae6643b39d9c42eaaac42d99e2441
Youneshimmi99 <himmiyounes99@gmail.com>
2023-05-01 23:36:16 +0100

    fix print listint

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -7,13 +7,15 @@
  */
 size_t print_listint(const listint_t *h)
 {
-	size_t num_nodes = 0;
+	int count;
 
+	count = 0;
 	while (h != NULL)
 	{
 		printf("%d\n", h->n);
 		h = h->next;
-		num_nodes++;
+		count++;
 	}
-	return (num_nodes);
+
+	return (count);
 }
