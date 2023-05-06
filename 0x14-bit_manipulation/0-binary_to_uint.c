6918a81f308bbaa25bfe3d561aa84910bb7f9752
Youneshimmi99 <himmiyounes99@gmail.com>
2023-05-06 02:22:36 +0100

    fix 0-binary_to_uint.c

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -10,24 +10,24 @@
 unsigned int binary_to_uint(const char *b)
 {
 	unsigned int i;
-	int lenght, base_two;
+	int l, base_two;
 
 	if (!b)
 		return (0);
 
 	i = 0;
 
-	for (lenght = 0; b[lenght] != '\0'; lenght++)
+	for (l = 0; b[l] != '\0'; l++)
 		;
 
-	for (lenght--, base_two = 1; lenght >= 0; lenght--, base_two *= 2)
+	for (l--, base_two = 1; l >= 0; l--, base_two *= 2)
 	{
-		if (b[lenght] != '0' && b[lenght] != '1')
+		if (b[l] != '0' && b[l] != '1')
 		{
 			return (0);
 		}
 
-		if (b[lenght] & 1)
+		if (b[l] & 1)
 		{
 			i += base_two;
 		}
