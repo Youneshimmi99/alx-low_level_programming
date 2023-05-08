88bffc3a016bab5c754bef9aee4bb41ceb1ad178
Youneshimmi99 <himmiyounes99@gmail.com>
2023-05-08 22:13:44 +0100

    refactor 0-read_textfile.c

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,49 +1,40 @@
 #include "main.h"
-#include <fcntl.h>
-#include <unistd.h>
-#include <stdlib.h>
-
 /**
- * read_textfile - reads a text file and prints it to the POSIX standard output
- * @filename: the name of the file to read
- * @letters: the number of letters to read and print
- *
- * Return: the actual number of letters read and printed, or 0 if an error occurs
- */
+  * read_textfile - read a text file and print to the POSIX standard
+  * output
+  * @filename: the file to read from
+  * @letters: the number of letters must be read and print
+  * Return: the actual number of letters to read and print
+  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	if (filename == NULL)
-		return (0);
+	char *buf;
+	int fd;
+	ssize_t n_read, n_written;
 
-	int fd = open(filename, O_RDONLY);
+	if (!filename)
+		return (0);
+	buf = malloc(sizeof(char) * letters);
+	if (!buf)
+		return (0);
+	fd = open(filename, O_RDONLY);
+
 	if (fd == -1)
-		return (0);
-
-	char *buffer = malloc(sizeof(char) * (letters + 1));
-	if (buffer == NULL)
 	{
-		close(fd);
+		free(buf);
 		return (0);
 	}
-
-	ssize_t n = read(fd, buffer, letters);
-	if (n == -1)
+	while ((n_read = read(fd, buf, letters)) > 0)
 	{
-		close(fd);
-		free(buffer);
-		return (0);
+		n_written = write(STDOUT_FILENO, buf, n_read);
+		if (n_written == -1 || n_written != n_read)
+		{
+			free(buf);
+			close(fd);
+			return (0);
+		}
 	}
-
-	buffer[n] = '\0';
-	ssize_t m = write(STDOUT_FILENO, buffer, n);
-	if (m == -1 || m != n)
-	{
-		close(fd);
-		free(buffer);
-		return (0);
-	}
-
+	free(buf);
 	close(fd);
-	free(buffer);
-	return (n);
+	return (n_written);
 }
